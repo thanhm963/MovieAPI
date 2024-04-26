42dcf5ac42dff76617982b5eee4bd0148b1f3825
Xuan Thanh <thanhm963@gmail.com>
2024-04-26 14:11:58 +0700

    1 review

--- src/main/java/dev/farhan/movieist/movies/Movie.java
+++ src/main/java/dev/farhan/movieist/movies/Movie.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -24,4 +25,6 @@ public class Movie {
     private String poster;
     private List<String> genres;
     private List<String> backdrops;
+    @DocumentReference
+    private List<Review> reviewIds;
 }
---
+++ src/main/java/dev/farhan/movieist/movies/Review.java
@@ -0,0 +1,17 @@
+package dev.farhan.movieist.movies;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id
+    private ObjectId id;
+    private String body;
+}
