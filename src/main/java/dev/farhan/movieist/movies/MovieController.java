375582b66f4c27992f1f152701f2a70be1f279da
Thanh Ma <thanhm963@gmail.com>
2024-04-26 14:58:25 +0700

    push 2

--- src/main/java/dev/farhan/movieist/movies/MovieController.java
+++ src/main/java/dev/farhan/movieist/movies/MovieController.java
@@ -2,14 +2,12 @@ package dev.farhan.movieist.movies;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 
 @RestController
-@RequestMapping("/api/v1/movíe")
+@RequestMapping("/api/v1/movies")
 
 public class MovieController {
     @GetMapping
--- src/main/java/dev/farhan/movieist/movies/Review.java
+++ src/main/java/dev/farhan/movieist/movies/Review.java
@@ -1,5 +1,7 @@
 package dev.farhan.movieist.movies;
 
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
