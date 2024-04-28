efb3880ae9f2730d894cfc83da0dadb23d047ccf
Thanh Ma <thanhm963@gmail.com>
2024-04-28 10:11:15 +0700

    4282024

--- src/main/java/dev/farhan/movieist/movies/MovieController.java
+++ src/main/java/dev/farhan/movieist/movies/MovieController.java
@@ -2,7 +2,18 @@ package dev.farhan.movieist.movies;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -10,9 +21,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequestMapping("/api/v1/movies")
 
 public class MovieController {
+    @Autowired
+    private MovieService movieService;
     @GetMapping
-    public String allMovies() {
-        return "All Movies";
+    public ResponseEntity<List<Movie>> getallMovies() {
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable ObjectId id ) {
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(id), HttpStatus.OK);
     
+}
 }
---
+++ src/main/java/dev/farhan/movieist/movies/MovieService.java
@@ -0,0 +1,22 @@
+package dev.farhan.movieist.movies;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public List<Movie> allMovies(){
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(ObjectId id){
+        return movieRepository.findById(id);
+    }
+}
